73dca99b5cdaf23d30bcf38b6a5fb9fbacb809f5
Tobias Eriksson <tobias.eriksson@cention.se>
2015-03-06 12:51:28 +0100

    Fix another problem with building ckeditor when directory is not Git repo

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -58,7 +58,7 @@ echo "Starting CKBuilder..."
 JAVA_ARGS=${ARGS// -t / } # Remove -t from arrgs
 
 VERSION="4.4.6 DEV"
-REVISION=$(git rev-parse --verify --short HEAD)
+REVISION=$(git rev-parse --verify --short HEAD 2>/dev/null) || true
 SEMVER_REGEX="^([0-9]+)\.([0-9]+)\.([0-9]+)(\-[0-9A-Za-z-]+)?(\+[0-9A-Za-z-]+)?$"
 
 # Get version number from tag (if available and follows semantic versioning principles).
